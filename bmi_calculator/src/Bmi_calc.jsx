c7c16835081a715e54edf92ca2e2c015efdb1ee7
Jaspreet Dhami <jdhami1@ualberta.ca>
2024-08-14 20:14:53 +0530

    bmi calculation complete. no css

--- bmi_calculator/src/Bmi_calc.jsx
+++ bmi_calculator/src/Bmi_calc.jsx
@@ -3,32 +3,33 @@ import { useState } from 'react';
 
 const Bmi_calc = () => {
   
-    const [weight,setWeight] = useState(0);
-    const [height,setHeight] = useState(0);
-    const [result,setResult] = useState(0);
+    const [weight,setWeight] = useState(70);
+    const [height,setHeight] = useState(180);
+    const [result,setResult] = useState(weight/(height/100)**2);
 
-    function handleCalc(){
-        let hm = height/100;
-        setResult(weight/(hm*hm));
+    function onWeightChange(event){
+        setWeight(event.target.value);
+        setResult(weight/(height/100)**2);
+    }
+
+    function onHeightChange(event){
+        setHeight(event.target.value);
+        setResult(weight/(height/100)**2);
     }
 
     return (
     <div>
         <h1>BMI Calculator</h1>
-        <form>
-            <div>
-            <label>Weight(kg): {weight}</label><br/>
-            <input type="range" id="weight" name="weight" min={0} max={120} step={1} value={weight} onChange={(e)=>{setWeight(e.target.value)}}/>
-            </div>
-            <div>
-            <label>Height(cm): {height}</label><br/>
-            <input type="range" id="height" name="height" min={0} max={220} step={1} value={height} onChange={(e)=>{setHeight(e.target.value)}}/>
-            </div>
-            <div>
-            <button onClick={handleCalc}>Calculate</button>
-            </div>
-        </form>
-        <div id="result">{result}</div>
+        <div className="input-section">
+            <p className="slider-output">Weight: {weight}kg</p>
+            <input type="range" className="input-slider" step="1" min="20" max="200" value={weight} onChange={onWeightChange}/>
+            <p className="slider-output">Height: {height}cm</p>
+            <input type="range" className="input-slider" step="1" min="50" max="250" value={height} onChange={onHeightChange}/>
+        </div>
+        <div className="output-section">
+            <p>Your BMI is:</p>
+            <p className="output">{result.toFixed(2)}</p>
+        </div>
     </div>
   )
 }
