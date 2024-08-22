5086c542248e11d9fe3993726eb6266911e67bb5
Jaspreet Dhami <jdhami1@ualberta.ca>
2024-08-22 13:42:49 +0530

    Added some tailwind css

--- projects/src/Weather.jsx
+++ projects/src/Weather.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Navigation from './navigation' 
 import './index.css'
-import { useState } from 'react'
+import './images/cold.jpg'
+import { useState, useRef } from 'react'
 
 const Weather = () => {
 
@@ -9,8 +10,10 @@ const Weather = () => {
         key: "16d49329caa5ce00157e0dbfe4b73c35",
         base: "https://api.openweathermap.org/data/2.5/"
     }
+
     const [place, setPlace] = useState('')
     const [weather, setWeather] = useState({})
+    const container = useRef(null)
 
     function handleChange(e) {
         setPlace(e.target.value)
@@ -23,32 +26,47 @@ const Weather = () => {
             .then(result => {
                 console.log(result)
                 setWeather(result)
+                backgroundChange(result.main.temp)
             })
 
         setPlace('')
         }
     }
+
+    function backgroundChange(temp){
+        console.log("Checking temp", temp)
+        if (temp < 16){
+            container.current.style.backgroundImage = "url('./images/cold.jpg')";
+        }
+        else if (temp >= 16 && temp < 25){
+            container.current.style.backgroundImage = "url('./images/cloudy.jpg')";
+        }
+        else{
+            container.current.style.backgroundImage = "url('./images/sunny.jpg')";
+        }
+    }
+
   return (
     <div>
         <Navigation />
-        <div id="container" className="p-16">
+        <div id="container" className="p-16 flex flex-col place-items-center h-screen" ref={container}>
             <div id="search">
-                <input type="text" id="search" placeholder="Enter city name" className="p-2 rounded-md border border-slate-400 text-black hover:shadow-md selection:border-slate-500"
+                <input type="text" id="search" placeholder="Enter city name" className="px-2 py-1 rounded-md border border-slate-400 text-black hover:shadow-md mb-10"
                 value={place} onChange={handleChange}
                 onKeyDown={handleKeyPress}/>
             </div>
-            <div id="weather-box" className="p-4">
+            <div id="weather-box" className="p-4 flex flex-col place-items-center">
                 {weather.main ? 
                 <>
-                <h3>{weather.name},{weather.sys.country}</h3>
+                <h3 className="text-3xl font-bold p-1">{weather.name},{weather.sys.country}</h3>
                 
-                <h6>Longitude:{weather.coord.lon} Latitude: {weather.coord.lat}</h6>
+                <h6 className="italic mb-5">Longitude:{weather.coord.lon} Latitude: {weather.coord.lat}</h6>
                 
-                <h2>{weather.main.temp}</h2>
+                <h2 className="p-8 bg-slate-100 font-bold text-5xl rounded-lg shadow-md shadow-stone-400 mb-5">{weather.main.temp}&deg;c</h2>
                 
-                <h3>{weather.weather[0].main}</h3>
+                <h3 className="text-2xl font-bold mb-1">{weather.weather[0].main}</h3>
                 
-                <h3>Wind: {weather.wind.speed} at {weather.wind.deg} Degrees with gust of {weather.wind.gust}</h3></> : 
+                </> : 
                 
                 <h2>Nothing to show yet</h2>}
                 
